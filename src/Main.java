ac5adef73d1725989dbb419748756d881bae74e1
Roksana Zalesskaya <Ksroksana@bk.ru>
2025-03-16 22:48:07 +0300

    Создала ветку dev. Создала в классе Main метод для разворота строки только в отношении буквенных символов.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input your string");
+        String input = scanner.nextLine();
+        String output = reverse(input);
+        System.out.println("New string is " + output);
+        scanner.close();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static String reverse(String str) {
+        if (str == null || str.isEmpty()) {
+            return "";
         }
+
+        char[] strArray = str.toCharArray();
+        int start = 0;
+        int end = str.length() - 1;
+        while (start < end) if (!Character.isLetter(strArray[start])) {
+            start++;
+        } else if (!Character.isLetter(strArray[end])) {
+            end--;
+        } else {
+            char temp = strArray[start];
+            strArray[start] = strArray[end];
+            strArray[end] = temp;
+            start++;
+            end--;
+        }
+
+        return new String(strArray);
     }
 }
