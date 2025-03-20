ba02812e0369c2fa050e9d42fbabf46a1996c93c
Roksana Zalesskaya <Ksroksana@bk.ru>
2025-03-20 20:22:57 +0300

    Убрала папку target в gitignore Написала тесты для четного/нечетного количества символов и одного символа во входящей строке Убрала тесты на if в while

--- src/test/java/com/example/utils/StringUtilTest.java
+++ src/test/java/com/example/utils/StringUtilTest.java
@@ -1,12 +1,13 @@
 package com.example.utils;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class StringUtilTest {
 
     @Test
-    public void ShouldReverseEmptyString() {
+    public void reverse_shouldReturnEmptyString_ifInputStringIsEmpty() {
         String input = "";
         String expected = "";
         String actual = StringUtil.reverse(input);
@@ -14,7 +15,7 @@ public class StringUtilTest {
     }
 
     @Test
-    public void ShouldReverseNullString() {
+    public void reverse_shouldReturnEmptyString_ifInputStringIsNull() {
         String input = null;
         String expected = "";
         String actual = StringUtil.reverse(input);
@@ -22,7 +23,7 @@ public class StringUtilTest {
     }
 
     @Test
-    public void ShouldReverseStringLetters() {
+    public void reverse_shouldReturnReverseString_ifInputContainsOnlyLetters() {
         String input = "asdf";
         String expected = "fdsa";
         String actual = StringUtil.reverse(input);
@@ -30,63 +31,18 @@ public class StringUtilTest {
     }
 
     @Test
-    public void ShouldReverseOnlyLetters() {
+    public void reverse_shouldReverseOnlyLetters_ifInputContainsNotOnlyLetters() {
         String input = "asdf123*//**";
         String expected = "fdsa123*//**";
         String actual = StringUtil.reverse(input);
         assertEquals(expected, actual);
     }
 
-   @Test
-    public void ShouldMoveToAnotherCharacterIfNoLettersAtStart() {
-        char[] strArray = "6java/ads".toCharArray();
-        int start = 0;
-        int end = strArray.length - 1;
-        while (start < end) {
-            if (!Character.isLetter(strArray[start])) {
-                start++;
-            } else {
-                break;
-            }
-        }
-        assertEquals(1, start);
-    }
-
     @Test
-    public void ShouldMoveToAnotherCharIfSymbolAtStart() {
-        char[] strArray = "*java/ads".toCharArray();
-        int start = 0;
-        int end = strArray.length - 1;
-        while (start < end) {
-            if (!Character.isLetter(strArray[start])) {
-                start++;
-            } else {
-                break;
-            }
-        }
-        assertEquals(1, start);
-    }
-
-    @Test
-    public void ShouldMoveToAnotherCharIfSpaceAtStart() {
-        char[] strArray = " java/ads".toCharArray();
-        int start = 0;
-        int end = strArray.length - 1;
-        while (start < end) {
-            if (!Character.isLetter(strArray[start])) {
-                start++;
-            } else {
-                break;
-            }
-        }
-        assertEquals(1, start);
-    }
-
-    @Test
-    public void ShouldMoveToEndIfNoLettersAtAll() {
+    public void reverse_shouldReverseOnlyLetters_ifInputContainsOnlySymbols() {
         char[] strArray = "*-+123".toCharArray();
         int start = 0;
-        int end = strArray.length-1;
+        int end = strArray.length - 1;
         while (start <= end) {
             if (!Character.isLetter(strArray[start])) {
                 start++;
@@ -95,5 +51,29 @@ public class StringUtilTest {
             }
         }
         assertEquals(strArray.length, start);
+    }
+
+    @Test
+    public void reverse_shouldReturnTheSameString_ifInputContainsOnlyOneLetter() {
+        String input = "a";
+        String expected = "a";
+        String actual = StringUtil.reverse(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverse_shouldReturnStringWithTheSameMiddle_ifInputContainsEven() {
+        String input = "a23kjl";
+        String expected = "l23jka";
+        String actual = StringUtil.reverse(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverse_shouldReturnReverseString_ifInputContainsOddSymbols() {
+        String input = "1234567ad";
+        String expected = "1234567da";
+        String actual = StringUtil.reverse(input);
+        assertEquals(expected, actual);
     }
 }
